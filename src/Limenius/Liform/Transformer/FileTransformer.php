8920e7e0c4bc5f7cdfc08389ba071c44c327369f
Ambroise Maupate <ambroise@rezo-zero.com>
2022-09-06 16:18:12 +0200

    feat: Added new FileTransformer to support FileType multiple option

---
+++ src/Limenius/Liform/Transformer/FileTransformer.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Limenius\Liform package.
+ *
+ * (c) Limenius <https://github.com/Limenius/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Limenius\Liform\Transformer;
+
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * @author Nacho Martín <nacho@limenius.com>
+ */
+class FileTransformer extends AbstractTransformer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(FormInterface $form, array $extensions = [], $widget = null)
+    {
+        $schema = ['type' => 'string'];
+        $schema = $this->addCommonSpecs($form, $schema, $extensions, $widget);
+        $schema = $this->addMultiple($form, $schema);
+
+        return $schema;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param array         $schema
+     *
+     * @return array
+     */
+    protected function addMultiple(FormInterface $form, array $schema)
+    {
+        if ($multiple = $form->getConfig()->getOption('multiple')) {
+            if (!isset($schema['attr'])) {
+                $schema['attr'] = [];
+            }
+            $schema['attr']['multiple'] = (bool) $multiple;
+        }
+
+        return $schema;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param array         $schema
+     * @param mixed         $configWidget
+     *
+     * @return array
+     */
+    protected function addWidget(FormInterface $form, array $schema, $configWidget)
+    {
+        $schema['widget'] = 'file';
+
+        return $schema;
+    }
+}
